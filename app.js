4ba620ace46f80360b3fc68ae0bddd83f6439280
victor ordax <victorordaxantolin@gmail.com>
2019-06-01 20:36:54 +0200

    cambio la extension de las plantillas de jade a hbs y redireccion al primer hbs de travels vacio

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var travelsRouter = require('./routes/travels');
 
 var hbs= require('hbs');
 var app = express();
@@ -13,7 +14,7 @@ var app = express();
 // view engine setup
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travels', travelsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
