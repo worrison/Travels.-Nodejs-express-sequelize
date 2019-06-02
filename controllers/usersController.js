54b366ee4b883a5a78b2ec04c8fecdc707ceafc8
victor ordax <victorordaxantolin@gmail.com>
2019-06-02 19:45:00 +0200

    consigue comprobar el usuario y loguearse

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -18,7 +18,23 @@ async function register(email,password,name){
     return models.user.create(user);
 }
 
+async function checkLogin(email, password){
+    
+    let user = await models.user.findAll({ where: { email: email } })
+    if(user.length === 0){
+        return null 
+    }else{
+        // Comparar contraseñas
+        let match = await bcrypt.compare(password, user[0].password);
+        console.log("resultado", match);
+        console.log("user", user[0].id);
+        return match ? user[0] : null;
+    }
+           
+}
+
 module.exports = {
     listUsers,
-    register
+    register,
+    checkLogin
 };
--- routes/users.js
+++ routes/users.js
@@ -10,32 +10,46 @@ router.get('/', async (req, res) => {
     });
 });
 
-router.get('/login', function (req, res) {
-    res.render('../views/users/login');
-});
+
+router.get('/login', (req, res) => {
+    let error = req.flash('errors');
+    if(req.session.name){
+      res.redirect('/');
+    }else{
+      res.render('../views/users/login', {
+        error
+      });
+    }
+  });
+
+  router.post('/login', async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    
+    if(!email || !password){
+      req.flash('errors', 'Falta usuario o contraseña');
+      res.redirect('/users/login')
+    } else {
+      let user = await usersController.checkLogin(email,password);
+      console.log("resultado controller",user.email)
+      if(user){
+        req.session.email = user.email;
+        req.session.name = user.name;
+        req.session.userId = user.id;
+        req.session.logginDate = new Date();
+        res.redirect('/travels');
+      }else{
+        req.flash('errors', 'Usuario o contraseña inválido');
+        res.redirect('/users/login');
+      }
+    }
+  
+  });
 
 router.get('/register', function (req, res) {
     res.render('../views/users/register');
 });
 
-// router.get('/login', (req, res) => {
-//     let error = req.flash('errors');
-//     if (req.session.name) {
-//         res.redirect('/');
-//     } else {
-//         res.render('..views/users/login', {
-//             error
-//         });
-//     }
-// })
-
-// router.get('/register', function (req, res) {
-//     //let error = req.flash('error');
-//     // res.render('..views/users/register', {
-//     //     error
-//     // });
-//     res.render('..views/users/register');
-// });
   router.post('/register', async (req, res) => {
     let { email, name, password} = req.body;
   
