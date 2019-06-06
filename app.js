a22ad9d1f38087ef3e7a10510311b6d20a1cb1c1
victor ordax <victorordaxantolin@gmail.com>
2019-06-06 16:15:36 +0200

    añadir a configuracion los cors

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ var logger = require('morgan');
 var session = require('express-session');
 var hbs= require('hbs');
 
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 /* icons import */
 // let library = require('@fortawesome/fontawesome-svg-core');
 // let fas = require('@fortawesome/free-solid-svg-icons'); 
