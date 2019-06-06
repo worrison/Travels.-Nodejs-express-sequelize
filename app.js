22f44285d6567010cea087f4f653a80c7d09bc21
victor ordaxm <victorordaxantolin@gmail.com>
2019-06-06 22:53:09 +0200

    cambio de posicion de la configuracion de los cors para permitir conectar con el front

--- app.js
+++ app.js
@@ -8,15 +8,6 @@ var session = require('express-session');
 var hbs= require('hbs');
 
 
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
 /* icons import */
 // let library = require('@fortawesome/fontawesome-svg-core');
 // let fas = require('@fortawesome/free-solid-svg-icons'); 
@@ -35,6 +26,14 @@ var ApiRouterTravels = require('./routes/Api/travel');
 
 
 var app = express();
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use(session({
   secret: 'miClaveSecreta',
